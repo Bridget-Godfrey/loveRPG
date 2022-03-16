8d3b6d2d22ff6f6dd693c3eaa2f99e24fa9b014b
Bridget-Godfrey <80221675+Bridget-Godfrey@users.noreply.github.com>
2022-03-16 15:55:28 -0400

    Made a faster collision checking algorithm

--- world.lua
+++ world.lua
@@ -12,7 +12,10 @@ end
 
 world = {}
 world.map = {}
+world.xy_map = {}
 world.mapSize = 0
+world.width = 1080 
+world.height = 720
 world.buildMap = {}
 world.drawAfter = {}
 world.loadMap = function ( mapNumber )
@@ -58,9 +61,18 @@ end
 
 world.loadTileInstances_ls = function (cycle)
 	if cycle == 0 then
+		for x = 0, math.floor(world.width/TILE_SIZE)+1 do
+			world.xy_map[x] = {}	
+			for y = 0, math.floor(world.height/TILE_SIZE)+1 do
+				world.xy_map[x][y] = {}	
+			end
+		end
 		return world.mapSize
 	elseif cycle <= world.mapSize then
-		table.insert(world.map, newTileInstance(world.buildMap[cycle][1], world.buildMap[cycle][2], world.buildMap[cycle][3], world.buildMap[cycle][4]))
+		local inst = newTileInstance(world.buildMap[cycle][1], world.buildMap[cycle][2], world.buildMap[cycle][3], world.buildMap[cycle][4])
+		table.insert(world.map, inst)
+		table.insert(world.xy_map[math.floor(world.buildMap[cycle][2]/TILE_SIZE)][math.floor(world.buildMap[cycle][3]/TILE_SIZE)], inst)
+
 		print ("created instance of " .. TILES[world.buildMap[cycle][1]].name)
 	else
 		--doNothing
@@ -69,6 +81,7 @@ world.loadTileInstances_ls = function (cycle)
 end
 world.endLoad = function ()
 	world.mapSize = table.getn(world.map)
+	
 end
 
 world.checkCollision = function (x, y, w, h)
@@ -76,6 +89,21 @@ end
 
 
 world.quickCheckCollision = function (x, y, w, h)
+	local mX, mY = convertWorldToTile(x, y)
+	local mX2 = math.min(mX+2, math.floor(world.width/TILE_SIZE))
+	 	   mX = math.max(mX-2, 0)
+	local mY2 = math.min(mY+2, math.floor(world.height/TILE_SIZE))
+	       mY = math.max(mY-2, 0)
+	for mtx = mX, mX2 do
+		for mty = mY, mY2 do
+			for i = 1, table.getn(world.xy_map[mtx][mty]) do
+				if checkTICollision(world.xy_map[mtx][mty][i], x, y, w, h) then
+					return true
+				end
+			end
+		end
+	end
+	return false
 end
 
 world.slowCheckCollision = function (x, y, w, h, startAt)
